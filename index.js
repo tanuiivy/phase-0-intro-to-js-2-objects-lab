2bb3900af01f23df81c182f35000531356204b0c
Ivy Tanui <ivy.tanui@strathmore.edu>
2024-12-20 20:42:17 +0300

    Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+// Initialize the employee object
+const employee = {
+    name: "Sam",
+    streetAddress: "123 Main St",
+  };
+  
+  // Function to update an employee non-destructively
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value,
+    }; 
+  }
+  
+  // Function to update an employee destructively
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; 
+    return employee;
+  }
+  
+  // Function to delete a key from an employee non-destructively
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee }; 
+    delete newEmployee[key]; 
+    return newEmployee;
+  }
+  
+  // Function to delete a key from an employee destructively
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; 
+    return employee;
+  }
+  
